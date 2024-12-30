2639fb2b340bb1df09292a9fd7f1e03edf92b11d
pooriyaKTB <pooriya.ketabi@gmail.com>
2024-12-30 15:39:37 +0000

    comments added, also did some clean up

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function setup() {
   makePageForEpisodes(allEpisodes);
 }
 
+// This function creates the content for the page using a list of episodes.
 function makePageForEpisodes(episodeList) {
   const show = document.getElementById("show-card");
   show.innerHTML = "";
@@ -11,11 +12,12 @@ function makePageForEpisodes(episodeList) {
   show.append(...episodeCards)
 }
 
-
+// This function creates an individual card for an episode using the template.
 const createEpisodeCard = (episode) => {
   const template = document.getElementById("episode-template");
   const card = template.content.cloneNode(true);
 
+  // Populate the card with episode details.
   card.querySelector(".episode-title").textContent = episode.name;
   card.querySelector(".episode-code").textContent = `S${String(episode.season).padStart(2,"0")}E${String(episode.number).padStart(2,"0")}`;
   card.querySelector(".episode-img").src = episode.image.medium ;
