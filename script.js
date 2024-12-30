e84198bb18523c78ef21cff2f6df53fac3f71a6d
pooriyaKTB <pooriya.ketabi@gmail.com>
2024-12-30 15:11:35 +0000

    script.js updated to display information about every episode of a single TV show

--- script.js
+++ script.js
@@ -5,8 +5,24 @@ function setup() {
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  const show = document.getElementById("show-card");
+  show.innerHTML = "";
+  const episodeCards = episodeList.map(createEpisodeCard);
+  show.append(...episodeCards)
 }
+
+
+const createEpisodeCard = (episode) => {
+  const template = document.getElementById("episode-template");
+  const card = template.content.cloneNode(true);
+
+  card.querySelector(".episode-title").textContent = episode.name;
+  card.querySelector(".episode-code").textContent = `S${String(episode.season).padStart(2,"0")}E${String(episode.number).padStart(2,"0")}`;
+  card.querySelector(".episode-img").src = episode.image.medium ;
+  card.querySelector(".episode-summary").innerHTML = episode.summary ;
+
+  return card;
+}
+
 
 window.onload = setup;
